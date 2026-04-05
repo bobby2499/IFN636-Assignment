ea311377dd65f97f3e2b47710c902153a8050e90
Bobby <bobby24992499@gmail.com>
2026-04-05 19:20:12 +1000

    update app.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,17 +3,29 @@ import Navbar from './components/Navbar';
 import Login from './pages/Login';
 import Register from './pages/Register';
 import Profile from './pages/Profile';
-import Tasks from './pages/Tasks';
+import Attractions from './pages/Attractions';
+import AttractionDetail from './pages/AttractionDetail';
+import AdminDashboard from './pages/admin/AdminDashboard';
+import AdminAttractions from './pages/admin/AdminAttractions';
+import AdminUsers from './pages/admin/AdminUsers';
+import AdminReviews from './pages/admin/AdminReviews';
 
 function App() {
   return (
     <Router>
       <Navbar />
       <Routes>
+        <Route path="/" element={<Attractions />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route path="/profile" element={<Profile />} />
-        <Route path="/tasks" element={<Tasks />} />
+        <Route path="/attractions" element={<Attractions />} />
+        <Route path="/attractions/:id" element={<AttractionDetail />} />
+        {/* Admin Routes */}
+        <Route path="/admin" element={<AdminDashboard />} />
+        <Route path="/admin/attractions" element={<AdminAttractions />} />
+        <Route path="/admin/users" element={<AdminUsers />} />
+        <Route path="/admin/reviews" element={<AdminReviews />} />
       </Routes>
     </Router>
   );
